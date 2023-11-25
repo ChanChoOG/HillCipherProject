d74f94b3196681d2f9c2ac52485ea839655a874f
ChanChoOG <nicolas.rod247@gmail.com>
2023-11-25 10:29:24 -0500

    Fixed cmd Menus

--- src/txt_src/txtDecrpt.py
+++ src/txt_src/txtDecrpt.py
@@ -13,8 +13,10 @@ def create_key_matrix(key):
     return np.array(matrix).reshape(3, 3)
 
 def decrypt_block(block, key_matrix):
-    # Calculate the modular inverse of the key matrix
     det = int(np.round(np.linalg.det(key_matrix)))
+    if np.gcd(det, 26) != 1:
+        raise ValueError("The key matrix is not invertible in modulo 26 arithmetic.")
+
     det_inv = pow(det, -1, 26)
     adjoint = np.round(np.linalg.inv(key_matrix) * det).astype(int)
     inverse_matrix = (det_inv * adjoint) % 26
@@ -26,14 +28,45 @@ def decrypt_block(block, key_matrix):
         decrypted_text += ''.join(num_to_char(num) for num in decrypted_vector)
     return decrypted_text
 
+
 def main():
-    key = 'WELOVEBAC'
+    print("Welcome to the Hill Cipher Decryption Program!")
+    print("-------------------------------------------------")
+
+    while True:
+        key = input('Enter the key used for encryption (Up to 9 characters): ').upper()
+
+        if len(key) <= 9:
+            key += 'X' * (9 - len(key))  # Fill the key with 'X's if it's too short
+            break
+        else:
+            print("Invalid key. The key must be 9 characters or less.")
+
     key_matrix = create_key_matrix(key)
 
-    encrypted_text = input('Enter the encrypted text: ').upper()
+    while True:
+        print("\nMenu:")
+        print("1. Decrypt a message")
+        print("2. Exit")
+        choice = input("Enter your choice (1-2): ")
 
-    decrypted_text = decrypt_block(encrypted_text, key_matrix)
-    print('Decrypted:', decrypted_text)
+        if choice == "1":
+            encrypted_text = input('\nEnter the encrypted text: ').upper()
+            decrypted_text = decrypt_block(encrypted_text, key_matrix)
+            print(f"\nDecrypted Message: {decrypted_text}")
+
+            # Summary
+            print("\nSummary:")
+            print(f"Key: {key}")
+            print(f"Encrypted Text: {encrypted_text}")
+            print(f"Decrypted Text: {decrypted_text}")
+
+        elif choice == "2":
+            print("\nExiting the decryption program.")
+            break
+
+        else:
+            print("\nInvalid choice. Please enter 1 or 2.")
 
 if __name__ == '__main__':
     main()
--- src/txt_src/txtEncrpt.py
+++ src/txt_src/txtEncrpt.py
@@ -4,7 +4,7 @@ def char_to_num(char):
     return ord(char) - ord('A')
 
 def num_to_char(num):
-    return chr(num + ord('A'))
+    return chr((num % 26) + ord('A'))
 
 def create_key_matrix(key):
     matrix = []
@@ -21,27 +21,48 @@ def encrypt_block(block, key_matrix):
     return encrypted_text
 
 def main():
+    print("Welcome to the Hill Cipher Encryption Program!")
+    print("-------------------------------------------------")
 
-    key = input('Enter the key you want to use: (Up to 9 characters)').upper()
-        #if key == '
+    while True:
+        key = input('Enter the key you want to use (Up to 9 characters): ').upper()
 
+        if len(key) <= 9:
+            key += 'X' * (9 - len(key))  # Fill the key with 'X's if it's too short
+            break
+        else:
+            print("Invalid key. The key must be 9 characters or less.")
 
     key_matrix = create_key_matrix(key)
 
     while True:
-        plaintext = input('Enter the plaintext (up to 9 characters) or type "exit" to quit: ').upper()
-        
-        if plaintext == "EXIT":
-            print("Exiting the program.")
+        print("\nMenu:")
+        print("1. Encrypt a message")
+        print("2. Exit")
+        choice = input("Enter your choice (1-2): ")
+
+        if choice == "1":
+            plaintext = input('\nEnter the plaintext (up to 9 characters): ').upper()
+            if len(plaintext) > 9:
+                plaintext = plaintext[:9]
+            elif len(plaintext) < 9:
+                plaintext += 'X' * (9 - len(plaintext))
+
+            encrypted_text = encrypt_block(plaintext, key_matrix)
+            print(f"\nEncrypted Message: {encrypted_text}")
+
+            # Summary
+            print("\nSummary:")
+            print(f"Key: {key}")
+            print(f"Original Plaintext: {plaintext}")
+            print(f"Encrypted Text: {encrypted_text}")
+
+        elif choice == "2":
+            print("\nExiting the encryption program.")
             break
 
-        if len(plaintext) > 9:
-            plaintext = plaintext[:9]
-        elif len(plaintext) < 9:
-            plaintext += 'X' * (9 - len(plaintext))
-
-        encrypted_text = encrypt_block(plaintext, key_matrix)
-        print('Encrypted:', encrypted_text)
+        else:
+            print("\nInvalid choice. Please enter 1 or 2.")
 
 if __name__ == '__main__':
     main()
